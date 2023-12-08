f7081575b2e9b7c6f9f76c067dc6341f3703e7b9
DivyaSri973 <bandaru.di@northeastern.edu>
2023-12-07 20:40:38 -0500

    changed layers.

--- PPOModel.py
+++ PPOModel.py
@@ -50,14 +50,15 @@ rewards = V2VRewards(-400, 300, -80, -500)
 class Policy(nn.Module):
     def __init__(self, state_size, action_size):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(state_size, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, action_size)
+        self.fc1 = nn.Linear(state_size, 32)
+        # self.fc2 = nn.Linear(64, 64)
+        self.fc2 = nn.Linear(32, action_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.sigmoid(self.fc3(x))
+        #x = F.sigmoid(self.fc3(x))
+        x = F.sigmoid(x)
         return x
 
 # Proximal Policy Optimization algorithm
